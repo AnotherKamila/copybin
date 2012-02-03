246d27bd1ce997a9322a7444731e79c105f7919c
Kamila Souckova <kamila@vesmir.sk>
2012-02-03 12:11:45 +0100

    adding some more option parsing

--- copybin.sh
+++ copybin.sh
@@ -3,9 +3,10 @@
 # copybin.sh -- a ssh pastebin without the "paste"
 # see http://github.com/anotherkamila/copybin
 
-# defaults
 CONFIG_FILE="$HOME/.config/copybinrc"
+# defaults
 VERBOSE=0
+PASSWDPROTECT=0
 
 usage() { # {{{
 cat << EOF
@@ -15,7 +16,6 @@ Copy files using scp, creating neat pastebin-like HTML along the way
 
 OPTIONS/TODOs:
   -h		Shows this message
-  -c FILE	Read config from FILE instead of default $CONFIG_FILE
   -p USER:PASS	password-protect the file with HTTP basic auth (.htpasswd)
 		(not implemented yet)
   -p USER	password-protect the file with HTTP basic auth (.htpasswd),
@@ -23,15 +23,12 @@ OPTIONS/TODOs:
 		(not implemented yet)
   -d		password-protect the file with HTTP basic auth (.htpasswd),
 		using the default USER and PASS from the config file
-		(not implemented yet)
   -r		rebuild all HTML documents (use after changing templates)
 		(not implemented yet)
   -v		Verbose
 		(not implemented yet)
 EOF
 } # }}}
-
-
 
 # options parsing
 while getopts ":hc:p:drv" OPTION; do
@@ -40,14 +37,13 @@ while getopts ":hc:p:drv" OPTION; do
 			usage
 			exit 1
 			;;
-		c)
-			[[ -n "$OPTARG" ]] && CONFIG_FILE="$OPTARG"
-			;;
 		p)
-			echo "u:p $OPTARG"
+			PASSWDPROTECT=1
+			USER=${OPTARG%:*}
+			if [[ $OPTARG == *:* ]]; then PASS=${OPTARG#*:}; else PASS=''; fi
 			;;
 		d)
-			echo "would encrypt w/ default u:p"
+			PASSWDPROTECT=1
 			;;
 		r)
 			echo "TODO"
@@ -63,7 +59,3 @@ while getopts ":hc:p:drv" OPTION; do
 	esac
 done
 shift $((OPTIND-1)) # leaves non-option arguments (i.e. files) in $@
-
-echo "CONFIG_FILE: $CONFIG_FILE"
-echo "VERBOSE: $VERBOSE"
-
