e7c034aa5797882f16404767a42b40d1d71e9927
Kamila Souckova <kamila@vesmir.sk>
2012-02-03 13:40:05 +0100

    add html generation

--- copybin.sh
+++ copybin.sh
@@ -3,10 +3,7 @@
 # copybin.sh -- a ssh pastebin without the "paste"
 # see http://github.com/anotherkamila/copybin
 
-CONFIG_FILE="$HOME/.config/copybinrc"
-# defaults
-VERBOSE=0
-PASSWDPROTECT=0
+# TODO support whole paths, not just plain filenames
 
 usage() { # {{{
 cat << EOF
@@ -29,9 +26,25 @@ OPTIONS/TODOs:
 		(not implemented yet)
 EOF
 } # }}}
+check_config() { # {{{
+	if [[ -z $SERVER || -z $HL_PROG || -z $TMPL ]]; then
+		echo "Required config options not present; exiting!" >&2
+		exit 127
+	fi
+	[[ $VERBOSE ]] && echo "Using config from file: $CONFIG_FILE"
+} # }}}
+
+# defaults
+if [[ -f "$COPYBIN_CONFIG_FILE" ]]; then
+	CONFIG_FILE="$COPYBIN_CONFIG_FILE"
+else
+	CONFIG_FILE="$HOME/.config/copybinrc"
+fi
+
+. $CONFIG_FILE
 
 # options parsing
-while getopts ":hc:p:drv" OPTION; do
+while getopts ":hc:p:dsrv" OPTION; do
 	case $OPTION in
 		h)
 			usage
@@ -44,6 +57,9 @@ while getopts ":hc:p:drv" OPTION; do
 			;;
 		d)
 			PASSWDPROTECT=1
+			;;
+		s)
+			[[ -n "$OPTARG" ]] && SERVER="$OPTARG"
 			;;
 		r)
 			echo "TODO"
@@ -58,4 +74,29 @@ while getopts ":hc:p:drv" OPTION; do
 			;;
 	esac
 done
+check_config
+
 shift $((OPTIND-1)) # leaves non-option arguments (i.e. files) in $@
+
+process() {
+	f="$1"
+	if [[ "$f" == */* ]]; then
+		echo "$f: Supporting paths is a TODO. For now skipping."
+		return 127
+	fi
+
+	TMPNAME="/tmp/copybin_${RANDOM}_${f}"
+	awk "{if(\$0==\"!CONTENT\"){system(\"$HL_PROG '$f'\")}else{print}}" "$TMPL" > "$TMPNAME"
+	sed -i -e "s/!NAME/$f/g" "$TMPNAME"
+
+	scp "$f" "$SERVER/$f"
+	scp "$TMPNAME" "$SERVER/$f.html"
+
+	# TODO pw
+
+	rm -f $TMPNAME
+}
+
+for f in $@; do
+	process "$f"
+done
