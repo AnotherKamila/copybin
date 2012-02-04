af423936512c015e3b33b08dea8c6c46a9093634
Kamila Souckova <kamila@vesmir.sk>
2012-02-04 18:54:43 +0100

    move htaccess content to template & fix passwords

--- copybin.sh
+++ copybin.sh
@@ -5,7 +5,6 @@
 
 # TODO support whole paths, not just plain filenames
 
-# runs the given function iff it is defined
 runif() { [[ `type -t $1` == function ]] && $1 ; }
 usage() { # {{{
 cat << EOF
@@ -16,36 +15,34 @@ Copy files using scp, creating neat pastebin-like HTML along the way
 OPTIONS/TODOs:
   -h		Shows this message
   -u USER:PASS	password-protect the file with HTTP basic auth (.htpasswd),
-		allowing only user USER with pw PASS to access it (a new user
-		is created in .htpasswd)
-		(not implemented yet)
+		allowing only user USER with pw PASS to access it
   -u USER	password-protect the file with HTTP basic auth (.htpasswd),
-		USER must already be present in the existing .htpasswd file
-		(not implemented yet)
+		allowing only user USER to access it (.htaccess is not updated)
   -a		password-protect the file with HTTP basic auth (.htpasswd),
-		using the 'Require valid-user' directive to allow all defined
-		users to access it
+		allowing all defined users to access it
   -s SERVER	specify the server string to be used with scp (ex.:
 		user@example.com:~/public_html/copybin/)
   -v		Verbose
-		(not implemented yet)
 EOF
 } # }}}
 check_config() { # {{{
+	[[ $VERBOSE ]] && echo "Using config from file: $CONFIG_FILE"
 	if [[ -z $SERVER || -z $HL_PROG || -z $TMPL ]]; then
 		echo "Required config options not present; exiting!" >&2
 		exit 127
 	fi
-	[[ $VERBOSE ]] && echo "Using config from file: $CONFIG_FILE"
 } # }}}
 
 # defaults {{{
+# if the env var $COPYBIN_CONFIG_FILE contains a valid file, use that instead of
+# the default
 if [[ -f "$COPYBIN_CONFIG_FILE" ]]; then
-	CONFIG_FILE="$COPYBIN_CONFIG_FILE"
+	export CONFIG_FILE="$COPYBIN_CONFIG_FILE"
 else
-	CONFIG_FILE="$HOME/.config/copybinrc"
+	export CONFIG_FILE="$HOME/.config/copybinrc"
 fi
 
+# `$CONFIG_FILE' can be used inside $CONFIG_FILE, because we have exported it
 . $CONFIG_FILE
 # }}}
 
@@ -77,37 +74,47 @@ while getopts ":hu:as:v" OPTION; do
 	esac
 done # }}}
 check_config
+# setup variables {{{
 shift $((OPTIND-1)) # leaves non-option arguments (i.e. files) in $@
+REMOTE="${SERVER%:*}"
+DIR="${SERVER#*:}"
+# }}}
 
-htpasswd() { # {{{
-	USER=$1; PASS=$2;
-	# create .htaccess if it doesn't exist {{{
-read -d '' HTACCESSCONTENT << "EOF"
-AuthType Basic
-AuthName \"Copybin: Authorization required\"
-AuthUserFile \"$HTPASSWD\"
-EOF
-read -d '' CMD << EOF
-if [ ! -f ${SERVER#*:}/.htaccess ]; then 
-	echo "$HTACCESSCONTENT" > ${SERVER#*:}/.htaccess
-fi
-EOF
-	# }}}
-
-	if [[ -n $PASS ]]; then
-		CMD="$CMD; htpasswd -bc ${SERVER#*:}/$HTPASSWD $USER $PASS"
+htaccess() { # {{{
+	f="$1"; REQUIRE="$2"
+	if [[ -z `ssh "$REMOTE" "test -f $DIR/.htaccess && echo 1"` ]]; then
+		[[ -n $VERBOSE ]] && echo ".htaccess not found on remote machine, copying template"
+		cp "$HTACCESS_TMPL_FILENAME" /tmp/copybin-htaccess
+		echo "AuthUserFile $HTPASSWD" >> /tmp/copybin-htaccess
+		scp /tmp/copybin-htaccess "$SERVER/.htaccess"
+		rm -f /tmp/copybin-htaccess
 	fi
 
-	ssh ${SERVER%:*} "$CMD"
+	ssh $REMOTE "[ -z \"\`grep $f $DIR/.htaccess\`\" ] && echo -e '<FilesMatch \"^$f(\.html)?$\">\n\tRequire $REQUIRE\n</FilesMatch>' >> $DIR/.htaccess"
 } # }}}
-htaccess() { # {{{
-ssh ${SERVER%:*} "[ -z \`grep $f ${SERVER#*:}/.htaccess\` ] && echo -e '<FilesMatch \"^$f(\.html)?$\">\n\tRequire $REQUIRE\n</FilesMatch>' >> ${SERVER#*:}/.htaccess"
+htpasswd() { # {{{
+	USER="$1"; PASS="$2"
+	if [[ -n $PASS ]]; then
+		ssh "$REMOTE" "htpasswd -bc $HTPASSWD $USER $PASS; chmod 644 $HTPASSWD"
+	fi
 } # }}}
 process() { # {{{
 	f="$1"
 	if [[ "$f" == */* ]]; then
 		echo "$f: Supporting paths is a TODO. For now skipping."
 		return 127
+	fi
+
+	if [[ -n $PASSWDPROTECT ]]; then
+		htpasswd $USER $PASS
+		if [[ -n $USER ]]; then
+			REQUIRE="user $USER"
+		else
+			REQUIRE='valid-user'
+		fi
+		htaccess "$f" "$REQUIRE"
+
+		[[ -n $VERBOSE ]] && echo "Protecting with Require $REQUIRE"
 	fi
 
 	TMPNAME="/tmp/copybin_${RANDOM}_${f}"
@@ -117,26 +124,14 @@ process() { # {{{
 	scp "$f" "$SERVER/$f"
 	scp "$TMPNAME" "$SERVER/$f.html"
 
-	if [[ -n $PASSWDPROTECT ]]; then
-		htpasswd $USER $PASS
-		if [[ -n "$USER" ]]; then
-			REQUIRE="user $USER"
-		else
-			REQUIRE='valid-user'
-		fi
-		htaccess $f $REQUIRE
-
-		[[ -n $VERBOSE ]] && echo "Protecting with Require $REQUIRE"
-	fi
-
-	rm -f $TMPNAME
+	rm -f "$TMPNAME"
 } # }}}
 
 runif beforeall
 for f in $@; do
-	runif beforefile $f
+	runif beforefile "$f"
 	process "$f"
-	runif afterfile $f
+	runif afterfile "$f"
 done
 runif afterall
 
